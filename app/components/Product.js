3497508e5c1880ffe462f21df3fcc7169dc8a6c3
Sanu Islam <sanuvipbd@gmail.com>
2023-05-22 21:05:53 +0600

    Solve Key Problem

--- app/components/Product.js
+++ app/components/Product.js
@@ -7,14 +7,14 @@ import Currency from "react-currency-formatter";
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
-const Product = ({ id, title, price, description, category, image }) => {
+const Product = ({ key, title, price, description, category, image }) => {
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
   const [hasPrime] = useState(Math.random() < 0.5);
 
   return (
-    <div key={id} className="relative flex flex-col m-5 bg-white z-30 p-10">
+    <div key={key} className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
 
       <Image src={image} height={200} width={200} objectFit="contain" alt="" />
