844afed30edb1a5d37a3565fcd7198e29e134388
Sanu Islam <sanuvipbd@gmail.com>
2023-05-22 21:03:11 +0600

    Change something

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -18,16 +18,16 @@ const Banner = () => {
         interval={5000}
       >
         <div>
-          <Image loading="Lazy" src="/assets/banner/banner1.jpg" alt=" " />
+          <Image loading="lazy" src="/assets/banner/banner1.jpg" alt=" " />
         </div>
         <div>
-          <Image loading="Lazy" src="/assets/banner/banner2.jpg" alt=" " />
+          <Image loading="lazy" src="/assets/banner/banner2.jpg" alt=" " />
         </div>
         <div>
-          <Image loading="Lazy" src="/assets/banner/banner3.jpg" alt=" " />
+          <Image loading="lazy" src="/assets/banner/banner3.jpg" alt=" " />
         </div>
         <div>
-          <Image loading="Lazy" src="/assets/banner/banner4.jpg" alt=" " />
+          <Image loading="lazy" src="/assets/banner/banner4.jpg" alt=" " />
         </div>
       </Carousel>
     </div>
